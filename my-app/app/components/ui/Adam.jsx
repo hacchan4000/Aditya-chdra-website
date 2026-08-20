1d38a75018c4364212399434e5689c58d0741b19
Aditya <adityanugraha7251@gmail.com>
2026-08-20 22:55:25 +0800

    add three js lib

---
+++ my-app/app/components/ui/Adam.jsx
@@ -0,0 +1,15 @@
+import * as THREE from 'three';
+
+const scene = new THREE.scene //tempat naro aset 3d
+const kamera = new THREE.PerspectiveCamera()
+
+const render = new THREE.WebGL3DRenderTarget()
+const Adam = () => {
+  return(
+    <div id="">
+
+    </div>
+  )
+}
+
+export default Adam
---
+++ my-app/app/test/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+Adam
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
